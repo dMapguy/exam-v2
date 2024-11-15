73e1fbc47dbf6ac9c71ad6039fa05f496d9bad44
dMapguy <153023723+dMapguy@users.noreply.github.com>
2024-11-15 16:23:53 +0000

    Initial commit

---
+++ astro.config.ts
@@ -0,0 +1,74 @@
+import { execSync } from "node:child_process";
+import node from "@astrojs/node";
+import tailwind from "@astrojs/tailwind";
+import { baremuxPath } from "@mercuryworkshop/bare-mux/node";
+// @ts-expect-error shut
+import { epoxyPath } from "@mercuryworkshop/epoxy-transport";
+// @ts-expect-error shut
+import { server as wisp } from "@mercuryworkshop/wisp-js/server";
+import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
+import { defineConfig } from "astro/config";
+import { viteStaticCopy } from "vite-plugin-static-copy";
+// https://astro.build/config
+export default defineConfig({
+  output: "hybrid",
+  adapter: node({
+    mode: "middleware",
+  }),
+  integrations: [tailwind({ applyBaseStyles: false })],
+  prefetch: {
+    defaultStrategy: "viewport",
+    prefetchAll: false,
+  },
+  image: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        pathname: "/UseInterstellar/**",
+      },
+    ],
+  },
+  vite: {
+    define: {
+      __COMMIT_DATE__: JSON.stringify(
+        execSync("git show --no-patch --format=%ci").toString().trim(),
+      ),
+    },
+    plugins: [
+      {
+        name: "vite-wisp-server",
+        configureServer(server) {
+          server.httpServer?.on("upgrade", (req, socket, head) =>
+            req.url?.startsWith("/f")
+              ? wisp.routeRequest(req, socket, head)
+              : undefined,
+          );
+        },
+      },
+      viteStaticCopy({
+        targets: [
+          {
+            src: `${epoxyPath}/**/*.mjs`.replace(/\\/g, "/"),
+            dest: "assets/bundled",
+            overwrite: false,
+            rename: (name) => `ex-${name}.mjs`,
+          },
+          {
+            src: `${baremuxPath}/**/*.js`.replace(/\\/g, "/"),
+            dest: "assets/bundled",
+            overwrite: false,
+            rename: (name) => `bm-${name}.js`,
+          },
+          {
+            src: `${uvPath}/**/*.js`.replace(/\\/g, "/"),
+            dest: "assets/bundled",
+            overwrite: false,
+            rename: (name) =>
+              `${name.replace("uv", "v").replace(/[aeiou]/gi, "")}.js`,
+          },
+        ],
+      }),
+    ],
+  },
+});
---
+++ index.ts
@@ -0,0 +1,138 @@
+import fs from "node:fs";
+import { createServer } from "node:http";
+import type { Socket } from "node:net";
+import path from "node:path";
+import fastifyMiddie from "@fastify/middie";
+import fastifyStatic from "@fastify/static";
+// @ts-expect-error shut
+import { server as wisp } from "@mercuryworkshop/wisp-js/server";
+import { build } from "astro";
+import Fastify from "fastify";
+import inConfig from "./config";
+
+const RenamedFiles: { [key: string]: string } = {};
+
+function RandomizeNames() {
+  const pagesDir = path.join(process.cwd(), "src", "pages");
+  const files = fs.readdirSync(pagesDir);
+
+  for (const file of files) {
+    if (file !== "index.astro" && file.endsWith(".astro")) {
+      const randomName = `${Math.random().toString(36).slice(2, 11)}.astro`;
+      const oldPath = path.join(pagesDir, file);
+      const newPath = path.join(pagesDir, randomName);
+      RenamedFiles[`/${file.replace(".astro", "")}`] =
+        `/${randomName.replace(".astro", "")}`;
+      fs.renameSync(oldPath, newPath);
+    }
+  }
+
+  const AstroFiles = FindAstroFiles(process.cwd());
+  for (const astroFile of AstroFiles) {
+    let fileContent = fs.readFileSync(astroFile, "utf-8");
+
+    for (const [oldName, newName] of Object.entries(RenamedFiles)) {
+      const regex = new RegExp(`"${oldName}"`, "g");
+      fileContent = fileContent.replace(regex, `"${newName}"`);
+    }
+
+    fs.writeFileSync(astroFile, fileContent, "utf-8");
+  }
+}
+
+function FindAstroFiles(dir: string): string[] {
+  let results: string[] = [];
+  const list = fs.readdirSync(dir);
+
+  for (const file of list) {
+    const resolvedFile = path.resolve(dir, file);
+    const stat = fs.statSync(resolvedFile);
+
+    if (stat?.isDirectory()) {
+      results = results.concat(FindAstroFiles(resolvedFile));
+    } else if (file.endsWith(".astro")) {
+      results.push(resolvedFile);
+    }
+  }
+  return results;
+}
+
+function Revert() {
+  const pagesDir = path.join(process.cwd(), "src", "pages");
+
+  for (const [oldPath, newPath] of Object.entries(RenamedFiles)) {
+    fs.renameSync(
+      path.join(pagesDir, `${newPath.replace("/", "")}.astro`),
+      path.join(pagesDir, `${oldPath.replace("/", "")}.astro`),
+    );
+  }
+
+  const AstroFiles = FindAstroFiles(process.cwd());
+  for (const astroFile of AstroFiles) {
+    let fileContent = fs.readFileSync(astroFile, "utf-8");
+
+    for (const [oldName, newName] of Object.entries(RenamedFiles)) {
+      const regex = new RegExp(`"${newName}"`, "g");
+      fileContent = fileContent.replace(regex, `"${oldName}"`);
+    }
+
+    fs.writeFileSync(astroFile, fileContent, "utf-8");
+  }
+}
+
+const port = Number.parseInt(process.env.PORT as string) || inConfig.port || 8080;
+const app = Fastify({
+  serverFactory: (handler) =>
+    createServer(handler).on("upgrade", (req, socket: Socket, head) =>
+      req.url?.startsWith("/f")
+        ? wisp.routeRequest(req, socket, head)
+        : socket.destroy(),
+    ),
+});
+
+// This is necessary to randomize the page names while preventing them from being committed by contributors.
+if (!fs.existsSync("dist")) {
+  RandomizeNames();
+  console.log("Interstellar's not built yet! Building now...");
+  await build({}).catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
+  Revert();
+}
+
+await app.register(import("@fastify/compress"), {
+  encodings: ["br", "gzip", "deflate"],
+});
+
+if (inConfig.auth?.challenge) {
+  await app.register(import("@fastify/basic-auth"), {
+    authenticate: true,
+    validate(username, password, _req, _reply, done) {
+      for (const [user, pass] of Object.entries(inConfig.auth?.users || {})) {
+        if (username === user && password === pass) {
+          return done();
+        }
+      }
+      return done(new Error("Invalid credentials"));
+    },
+  });
+  await app.after();
+  app.addHook("onRequest", app.basicAuth);
+}
+
+// @ts-ignore
+const { handler } = await import("./dist/server/entry.mjs");
+await app
+  .register(fastifyStatic, {
+    root: path.join(import.meta.dirname, "dist", "client"),
+  })
+  .register(fastifyMiddie);
+app.use(handler);
+app.listen({ port }, (err, addr) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log("Listening on %s", addr);
+});
